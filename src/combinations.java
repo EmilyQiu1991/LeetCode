3a23d9bc3d358a5e4a435351691757fb6c80a842
EmilyQiu1991 <867284031@qq.com>
2017-07-05 20:56:40 -0500

    to 4

--- src/combinations.java
+++ src/combinations.java
@@ -3,7 +3,7 @@ public class combinations {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test branch 1");
+		System.out.println("test branch 4");
 	}
 	
 
