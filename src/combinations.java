990facb96ac476b1837949d9e20a545f543192fe
EmilyQiu1991 <867284031@qq.com>
2017-07-05 22:12:22 -0500

    first commit

--- src/combinations.java
+++ src/combinations.java
@@ -2,12 +2,9 @@
 public class combinations {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-<<<<<<< HEAD
-		System.out.println("test branch 4");
-=======
-		System.out.println("test branch 3");
->>>>>>> vyrd67
+
+		System.out.println("test branch 1");
+
 	}
 	
 
