c6a003a00b05829bba9380f18c8ad43b893587b5
EmilyQiu1991 <867284031@qq.com>
2017-07-05 21:05:22 -0500

    ftg

--- src/combinations.java
+++ src/combinations.java
@@ -3,7 +3,11 @@ public class combinations {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+<<<<<<< HEAD
 		System.out.println("test branch 4");
+=======
+		System.out.println("test branch 3");
+>>>>>>> vyrd67
 	}
 	
 
